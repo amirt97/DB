729af06db9d9af8a3ec0afb7dfeb1fd377f7f3a5
amirt97 <amir.tuboul@mail.huji.ac.il>
2025-04-03 17:57:39 +0300

    updated creat and drop

--- Ex-1/create.sql
+++ Ex-1/create.sql
@@ -1,19 +1,43 @@
-create table enrollment(
-	country varchar, 
-	countrycode varchar, 
-	region varchar, 
-	incomegroup varchar, 
-	iau_id1 varchar, 
-	eng_name varchar, 
-	orig_name varchar, 
-	foundedyr varchar, 
-	yrclosed varchar, 
-	private01 varchar, 
-	latitude varchar, 
-	longitude varchar, 
-	phd_granting varchar, 
-	divisions varchar, 
-	specialized varchar, 
-	year varchar, 
-	students5_estimated varchar
+create table Region(
+	region varchar(40) primary key
 );
+
+create table Incomegroup(
+	incomegroup varchar(30) primary key
+);
+
+create table Coutnry(
+	countrycode char(3) primary key,
+	country varchar,
+	region varcha(40),
+	incomegroup varchar(30)
+	foreign key(region) refrences Region(region),
+	foreign key(incomegroup) refrences Incomegroup(incomegroup)
+);
+
+create table Unveristy(
+	iau_id1 varchar primary key, 
+	eng_name varchar not null, 
+	orig_name varchar not null, 
+	foundedyr integer check(foundedyr>0) not null, 
+	yrclosed integer check(yrclosed>0), 
+	private01 varchar, 
+	latitude float, 
+	longitude float, 
+	phd_granting boolean not null, 
+	divisions integer, 
+	specialized boolean not null, 
+	year integer,
+	countrycode char(3) not null,
+	foreign key(countrycode) refrences Country(countrycode)
+
+);
+
+
+create table enrollmentData(
+	iau_id1 varchar primary key,
+	year integer primary key,
+	students5_estimated integer,
+	foreign key(iau_id1) refrences Unveristy(iau_id1)
+);
+
--- Ex-1/drop.sql
+++ Ex-1/drop.sql
@@ -1,1 +1,5 @@
-drop table enrollment;
+drop table if exists Incomegroup cascade;
+drop table if exists Region cascade;
+drop table if exists Country cascade;
+drop table if exists University cascade;
+drop table if exists EnrollmentData cascade;
