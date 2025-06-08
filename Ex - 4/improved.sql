466c36d306c5bac33588e2eb890a83975407aece
amirt97 <amir.tuboul@mail.huji.ac.il>
2025-06-08 15:15:20 +0300

    updated improved file

--- Ex - 4/improved.sql
+++ Ex - 4/improved.sql
@@ -1,8 +1,9 @@
-SELECT DISTINCT e1.year, e1.eng_name
-FROM enrollment e1
-WHERE (year, students5_estimated) IN (
-    SELECT year, MAX(students5_estimated)
-    FROM enrollment e2
+EXPLAIN ANALYZE
+SELECT e1.year, e1.name, e1.state
+FROM elected_officials e1
+JOIN (
+    SELECT year, MAX(votes) AS max_votes
+    FROM elected_officials
     GROUP BY year
-)
-ORDER BY year, eng_name;
+) e2 ON e1.year = e2.year AND e1.votes = e2.max_votes
+ORDER BY e1.year, e1.name;
